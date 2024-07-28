6478ecd1f8d1b5fad5c71a5c2b4ef843771d6f5e
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-07-28 22:28:33 +0100

    Create update.js

---
+++ plugins/update.js
@@ -0,0 +1,142 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from'fs';
+import path from'path';
+import axios from'axios';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const owner = 'noureddineouafy';
+const repo = 'silana-bot';
+const handler = async (m, { text, usedPrefix, command }) => {
+
+
+if (!text) {
+   const folders = ['plugins', 'lib']; 
+// مجلدات لي غادين يتحدثو بالامر ديريكت
+
+   function generateRandomIP() {
+     return Math.floor(Math.random() * 256) + '.' +
+       Math.floor(Math.random() * 256) + '.' +
+       Math.floor(Math.random() * 256) + '.' +
+       Math.floor(Math.random() * 256);
+   }
+
+   function fetchAndSaveFiles(folder) {
+     const githubApiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${folder}`;
+     const rootPath = path.join(__dirname, '..', folder);
+
+     axios.get(githubApiUrl, {
+       headers: {
+         'X-Forwarded-For': generateRandomIP()
+       }
+     })
+       .then(response => {
+         const files = response.data;
+
+         if (Array.isArray(files)) {
+           if (!fs.existsSync(rootPath)) {
+             fs.mkdirSync(rootPath, { recursive: true });
+             console.log(`Folder created: ${folder}`);
+
+           }
+
+           files.forEach(file => {
+             if (file.type === 'file' && file.name !== 'update.js') {
+               const filePath = path.join(rootPath, file.name);
+
+               axios.get(file.download_url, { responseType: 'arraybuffer', headers: { 'X-Forwarded-For': generateRandomIP() } })
+                 .then(response => {
+                   fs.writeFile(filePath, response.data, err => {
+                     if (err) throw err;
+                     console.log(`File saved: ${file.name}`);
+
+
+                   });
+                 })
+                 .catch(err => {
+                   console.error(`Error downloading file: ${file.name}`, err);
+                 });
+             }
+           });
+         } else {
+           console.log(`The folder '${folder}' does not exist in the repository.`);
+
+         }
+       })
+       .catch(err => {
+         if (err.response && err.response.status === 404) {
+           console.log(`The folder '${folder}' does not exist in the repository.`);
+         } else {
+           console.error(`Error fetching from GitHub API for folder ${folder}`, err);
+ conn.reply(m.chat, `*اعد المحاولة بعد دقيقة* !!`, m);
+         }
+       });
+   }
+
+   folders.forEach(folder => {
+     fetchAndSaveFiles(folder);
+   });
+   conn.reply(m.chat, `*تم تحديث روبوتك*🥳`, m);
+
+} else {
+   const files = [text];
+   function generateRandomIP() {
+     return Math.floor(Math.random() * 256) + '.' +
+       Math.floor(Math.random() * 256) + '.' +
+       Math.floor(Math.random() * 256) + '.' +
+       Math.floor(Math.random() * 256);
+   }
+
+   function fetchAndSaveFile(filePath) {
+     const githubApiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${filePath}`;
+     const localFilePath = path.join(__dirname, '..', filePath);
+
+     axios.get(githubApiUrl, {
+       headers: {
+         'X-Forwarded-For': generateRandomIP()
+       }
+     })
+       .then(response => {
+         const data = response.data;
+
+         if (data.type === 'file' && filePath !== 'plugins/update.js') {
+           axios.get(data.download_url, { responseType: 'arraybuffer', headers: { 'X-Forwarded-For': generateRandomIP() } })
+             .then(response => {
+               fs.writeFile(localFilePath, response.data, err => {
+                 if (err) throw err;
+                 console.log(`File saved: ${filePath}`);
+conn.reply(m.chat, `*تم تحديث روبوتك* 🥳`, m);
+               });
+             })
+             .catch(err => {
+               console.error(`Error downloading file: ${filePath}`, err);
+             });
+         } else {
+           console.log(`The file '${filePath}' does not exist or is not of type 'file'.`);
+ 
+conn.reply(m.chat, `*الملف ${filePath} غير موجود*!!`, m);
+         }
+       })
+       .catch(err => {
+         if (err.response && err.response.status === 404) {
+           console.log(`The file '${filePath}' does not exist in the repository.`);
+ 
+  conn.reply(m.chat, `*الملف ${filePath} غير موجود*!!`, m);
+         } else {
+           console.error(`Error fetching from GitHub API for file ${filePath}`, err);
+ conn.reply(m.chat, `*انتظر دقيقة ثم اعد المحاولة* ❤️`, m);
+         }
+       });
+   }
+
+   files.forEach(file => {
+     fetchAndSaveFile(file);
+   });
+}
+
+};     
+handler.command = /^up(date)?f?$/i;   
+export default handler
+
